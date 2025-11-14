a65b0e0ff191cd2a75d8b9149330ee7f2db954ed
Thomas Parratt <thomasparratt@googlemail.com>
2025-11-14 22:03:55 +0200

    EOD

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -22,7 +22,7 @@ export default function BookingForm({addBooking}) {
         <form className="flex flex-col gap-2 border rounded p-4 bg-white" onSubmit={handleSubmit}>
             <h3 className="font-semibold">Add Booking</h3>
             <input className="border px-2 py-1 rounded" placeholder="Title" value={title} onChange={e=>setTitle(e.target.value)} />
-            <input className="border px-2 py-1 rounded" type="date" value={date} onChange={e=>setDate(e.target.value)} />Date
+            <input className="border px-2 py-1 rounded" type="date" value={date} onChange={e=>setDate(e.target.value)} />
             <input className="border px-2 py-1 rounded" type="time" value={startTime} onChange={e=>setStartTime(e.target.value)} />Start
             <input className="border px-2 py-1 rounded" type="time" value={endTime} onChange={e=>setStartTime(e.target.value)} />End
             <button className="bg-gray-900 text-white px-3 py-1 rounded mt-2" type="submit">Add</button>
